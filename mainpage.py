bf4989d61496d88be7879e0d9fc996f46e719e1c
st21233 <105762906+st21233@users.noreply.github.com>
2023-08-14 11:32:55 +1200

    Merged resultpage with mainpage.

--- mainpage.py
+++ mainpage.py
@@ -31,6 +31,38 @@ free_throw_failed_entry = tk.Entry(frame)
 free_throw_failed_entry.grid(row=5, column=1)
 
 
+def display_results(blocks, turnovers, steals, assists, free_throw_success, free_throw_failed):
+     # Create a new Tkinter window
+     window = tk.Tk()
+     window.title("Basketball Analysis Results")
+      
+     # Create a frame to hold the table that will display the results
+     frame = tk.Frame(window)
+     frame.pack()
+     
+     #Create a table and add rows and columns for the data that will be displayed
+     table = tk.Treeview(frame, columns=("Blocks", "Turnovers", "Steals", "Assists", "Free Throw Success", "Free Throw Failed"))
+     
+     # Set the column headings
+     table.heading("#1", text="Blocks")
+     table.heading("#2", text="Turnovers")
+     table.heading("#3", text="Steals")
+     table.heading("#4", text="Assists")
+     table.heading("#5", text="Free Throw Success")
+     table.heading("#6", text="Free Throw Failed")
+     
+     table.grid(row=0, column=0)
+     
+     # Add data to the table
+     table.insert("", "end", values=(blocks, turnovers, steals, assists, free_throw_success, free_throw_failed))
+     
+     # Create a button to close the window
+     close_button = tk.Button(frame, text="Close", command=window.destroy)
+     close_button.grid(row=1, column=0)
+     
+     
+     
+
 # Create a function to process the data entered by the user
 def submit():
     # Get the data from the input fields
@@ -40,6 +72,9 @@ def submit():
     assists = assists_entry.get()
     free_throw_success = free_throw_success_entry.get()
     free_throw_failed = free_throw_failed_entry.get()
+    
+    display_results(blocks, turnovers, steals, assists, free_throw_success, free_throw_failed)
+
 
 # Create a submit button
 submit_button = tk.Button(frame, text="submit", command=submit)
@@ -47,4 +82,9 @@ submit_button.grid(row=6, column=0)
 blocks_entry = tk.Entry(frame)
 
 blocks_entry.grid(row=0, column=1)
-submit()
+
+# set window size
+tk.geometry("500x200")
+
+# Run the mainloop
+window.mainloop()
